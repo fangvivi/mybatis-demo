5ac02f74bb3a29180e6289b635e2d059d3e704d5
kernel <fangwaine@outlook.com>
2022-08-08 18:13:43 +0800

    动态SQL

--- mybatis-demo-03/src/main/java/com/wayne/mybatis/mapper/DynamicSQLMapper.java
+++ mybatis-demo-03/src/main/java/com/wayne/mybatis/mapper/DynamicSQLMapper.java
@@ -1,6 +1,7 @@
 package com.wayne.mybatis.mapper;
 
 import com.wayne.mybatis.pojo.Emp;
+import org.apache.ibatis.annotations.Param;
 
 import java.util.List;
 
@@ -9,10 +10,34 @@ import java.util.List;
  * @author wayne
  */
 public interface DynamicSQLMapper {
+
     /**
-     * if标签，多条件查询，获取员工信息
+     * 多条件查询，获取员工信息
+     * 测试if、where、trim标签
      * @param emp 查询条件
      * @return 查询到的员工信息
      */
     List<Emp> getEmpByCondition(Emp emp);
+
+    /**
+     * 多条件查询，获取员工信息
+     * 测试choose标签
+     * @param emp 查询条件
+     * @return 查询到的员工信息
+     */
+    List<Emp> getEmpByChoose(Emp emp);
+
+    /**
+     * 批量删除
+     * @param eids 需要删除的员工id
+     * @return 影响的行数
+     */
+    int deleteMoreByArray(@Param("eids") Integer[] eids);
+
+    /**
+     * 批量插入
+     * @param emps 需要插入的员工信息
+     * @return 影响的行数
+     */
+    int insertMoreByList(@Param("emps") List<Emp> emps);
 }
--- mybatis-demo-03/src/test/java/com/wayne/mybatis/mapper/DynamicSQLMapperTest.java
+++ mybatis-demo-03/src/test/java/com/wayne/mybatis/mapper/DynamicSQLMapperTest.java
@@ -9,6 +9,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
+import java.util.Arrays;
 import java.util.List;
 
 /**
@@ -22,9 +23,52 @@ public class DynamicSQLMapperTest {
         try(final SqlSession sqlSession = SqlSessionUtils.getSqlSession()){
             final DynamicSQLMapper mapper = sqlSession.getMapper(DynamicSQLMapper.class);
             final List<Emp> empList =
-                    mapper.getEmpByCondition(new Emp("", 23, "", ""));
+                    mapper.getEmpByCondition(new Emp("", 23, "", "123@qq.com"));
             Assert.assertNotEquals(0,empList.size());
             log.info("{}", empList);
+        } catch (IOException e) {
+            log.error("{}", e.getMessage(), e);
+        }
+    }
+
+    @Test
+    public void testGetEmpByChoose(){
+        try(final SqlSession sqlSession = SqlSessionUtils.getSqlSession()){
+            final DynamicSQLMapper mapper = sqlSession.getMapper(DynamicSQLMapper.class);
+            //实际查出来的结果： [Emp{eid=1, empName='张三', age=23, sex='男', email='123@qq.com', dept=null}]
+            final List<Emp> empList =
+                    mapper.getEmpByChoose(new Emp("张三", 32, "女", "12312@qq.com"));
+            Assert.assertNotEquals(0,empList.size());
+            log.info("{}", empList);
+        } catch (IOException e) {
+            log.error("{}", e.getMessage(), e);
+        }
+    }
+
+    @Test
+    public void testDeleteMoreByArray(){
+        try(final SqlSession sqlSession = SqlSessionUtils.getSqlSession()){
+            final DynamicSQLMapper mapper = sqlSession.getMapper(DynamicSQLMapper.class);
+            //实际查出来的结果： [Emp{eid=1, empName='张三', age=23, sex='男', email='123@qq.com', dept=null}]
+            final int count = mapper.deleteMoreByArray(new Integer[]{4,5});
+            Assert.assertNotEquals(0,count);
+            log.info("{}", count);
+        } catch (IOException e) {
+            log.error("{}", e.getMessage(), e);
+        }
+    }
+
+    @Test
+    public void testInsertMoreByArray(){
+        try(final SqlSession sqlSession = SqlSessionUtils.getSqlSession()){
+            final DynamicSQLMapper mapper = sqlSession.getMapper(DynamicSQLMapper.class);
+            Emp emp1 = new Emp("a1",23,"男","123@qq.com");
+            Emp emp2 = new Emp("a2",23,"男","123@qq.com");
+            Emp emp3 = new Emp("a3",23,"男","123@qq.com");
+            List<Emp> empList = Arrays.asList(emp1,emp2, emp3);
+            final int count = mapper.insertMoreByList(empList);
+            Assert.assertNotEquals(0,count);
+            log.info("{}", count);
         } catch (IOException e) {
             log.error("{}", e.getMessage(), e);
         }
